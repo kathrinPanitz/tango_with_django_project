b978db35f4e938309bdcd8b9a8580fb619f42fdb
Kathrin Panitz <2411521P@student.gla.ac.uk>
2019-01-10 19:12:12 +0100

    project setup and first url mapping

---
+++ rango/urls.py
@@ -0,0 +1,13 @@
+
+
+# This code imports the relevant Django machinery for URL mappings
+# and the views module from rango.
+# This allows us to call the function url and point to the index view
+# for mapping in urlpatterns. 
+
+from django.conf.urls import url
+from rango import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
