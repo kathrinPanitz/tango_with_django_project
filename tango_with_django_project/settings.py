01e038d316d5311791dd49e1ad8f1776ab730c7b
Kathrin Panitz <2411521P@student.gla.ac.uk>
2019-01-16 19:27:48 +0100

    chapter 4.2 - static media file/ image

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Add the dynamic template directory
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# Variable for static directory
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -118,5 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
