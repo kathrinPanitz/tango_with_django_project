252a845dd32feea9fa21f3320c912fff650a0fd8
Kathrin Panitz <2411521P@student.gla.ac.uk>
2019-01-16 18:38:12 +0100

    Third chapter

--- rango/urls.py
+++ rango/urls.py
@@ -3,11 +3,12 @@
 # This code imports the relevant Django machinery for URL mappings
 # and the views module from rango.
 # This allows us to call the function url and point to the index view
-# for mapping in urlpatterns. 
+# for mapping in urlpatterns.
 
 from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# This is the method for the index
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+# The following code is a new view method called about
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
